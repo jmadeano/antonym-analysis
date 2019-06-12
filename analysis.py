017ed1ab13a2246b9c418759c2879b1214e0201d
jmadeano <jmadeano@mit.edu>
2019-06-12 16:49:48 -0400

    Analyze fastText and gloVe for morphological antonyms

--- analysis.py
+++ analysis.py
@@ -6,4 +6,37 @@ from pprint import pprint
 with open('data-as-dict.json', 'r') as outfile:
     data_map = json.load(outfile)
 
-pprint(data_map)
+# pprint(data_map)
+
+# Occurences of morphological antonyms in word-embedding models
+
+def find_morph_ants_in_similar_words(model):
+    morph_ants_in_sim_words = {}
+
+    for (word, ants) in model.items():
+        for ant in ants.keys():
+            if is_morphological_negation(word, ant):
+                morph_ants_in_sim_words[word] = True
+
+    return morph_ants_in_sim_words
+
+
+
+
+fasttext_morph_ants = find_morph_ants_in_similar_words(data_map['model-fasttext'])
+glove_morph_ants = find_morph_ants_in_similar_words(data_map['model-glove'])
+
+pprint(fasttext_morph_ants)
+print('\n\n')
+pprint(glove_morph_ants)
+
+
+# Findings:
+# morph_ants are very common in fastText model (23/30)
+# much less common in glove (7/30)
+
+
+
+
+# When I return, look at:
+# http://propor2016.di.fc.ul.pt/wp-content/uploads/2016/07/BrunaThalenbergPROPORSRW2016.pdf
