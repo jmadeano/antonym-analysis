44357e67abc2860a42466b9963ac78335fdeb2d5
jmadeano <jmadeano@mit.edu>
2019-06-13 15:11:05 -0400

    Move find_morphological_antonyms to helpers.py, add documentation

--- analysis.py
+++ analysis.py
@@ -9,25 +9,13 @@ with open('data-as-dict.json', 'r') as outfile:
 # pprint(data_map)
 
 # Occurences of morphological antonyms in word-embedding models
-def find_morph_ants_in_similar_words(model):
-    morph_ants_in_sim_words = {}
-
-    for (word, ants) in model.items():
-        for ant, sim in ants.items():
-            if is_morphological_negation(word, ant):
-                morph_ants_in_sim_words[word] = sim
-
-    return morph_ants_in_sim_words
-
-# ADD: ab-normal, a-typical
-
-
-fasttext_morph_ants = find_morph_ants_in_similar_words(data_map['model-fasttext'])
-glove_morph_ants = find_morph_ants_in_similar_words(data_map['model-glove'])
+fasttext_morph_ants = find_morphological_antonyms(data_map['model-fasttext'])
+glove_morph_ants = find_morphological_antonyms(data_map['model-glove'])
 
 pprint(fasttext_morph_ants)
 print('\n\n')
 pprint(glove_morph_ants)
+print('\n\n')
 
 # Findings:
 # morph_ants are very common in fastText model (23/30)
@@ -36,22 +24,7 @@ pprint(glove_morph_ants)
 
 
 # Occurences of morphological antonyms in data
-# Note this does not check if it is the CORRECT morphological negation
-# compassionate => incompassionate vs uncompassionate, still counts as morph ant
-def find_morph_ants_in_data(data, all = False): # all means look for all morphological negations (not just of original word)
-    morph_ants_in_sim_words = {}
-
-    for (word, ants) in data.items():
-        for ant, prob in ants.items():
-            if is_morphological_negation(word, ant):
-                morph_ants_in_sim_words[word] = prob
-
-    return morph_ants_in_sim_words
-
-
-
-print('\n\n')
-data = find_morph_ants_in_data(data_map['data-full'])
+data = find_morphological_antonyms(data_map['data-full'])
 pprint(data)
 print(len(data))
 
