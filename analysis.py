23ad87298f7515e0ce1b0468debe006ee10c9c21
jmadeano <jmadeano@mit.edu>
2019-06-25 08:30:31 -0400

    Check correlation between trans_prob and word frequency, separate all morphological responses

--- analysis.py
+++ analysis.py
@@ -2,6 +2,9 @@ from helpers import *
 from pprint import pprint
 from scipy import stats
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 # Analysis should always start with reading from a file. To update models or seed
 #   words, go to update-models.py
@@ -36,7 +39,6 @@ data = find_morphological_antonyms(data_map['data-full'])
 # Comparing word frequency in antonym choices
 word_freq = find_word_freq(data_map['data-full'])
 
-pprint(word_freq)
 
 
 col = 'freq-absolute'
@@ -65,7 +67,7 @@ test = pd.read_csv(filename)
 # Detect terminal width for dataframe printing
 pd.options.display.width = 0
 
-# Keep only these three fields
+# Keep only these three fields... Be careful (names will change after experiment)
 filtered = test[['positive', 'antonym', 'response']]
 
 # Strip leading/trailing whitespace and lowercase all stimuli and responses
@@ -83,7 +85,10 @@ sorted  = sorted.drop_duplicates()
 
 # Calculate transition probability: specific_antonym.count()/all_antonyms.count()
 sorted = sorted.assign(trans_prob = sorted.groupby('positive').transform(lambda x: x/x.sum()))
-sorted = sorted.reset_index(drop = True)
+sorted = sorted.reset_index(drop = True)# Add bool column for morphological antonyms
+
+# Add bool column for morphological antonyms
+sorted = sorted.assign(is_morph = sorted['response'] == sorted['antonym'])
 
 # Add word frequencies for response words
 sorted = sorted.assign(freq_absolute = sorted['response'].apply(lambda word: zipf_frequency(word, 'en')))
@@ -91,11 +96,31 @@ sorted = sorted.assign(freq_absolute = sorted['response'].apply(lambda word: zip
 # Add relative word frequencies: response_freq - stimuli_freq
 sorted = sorted.assign(freq_relative = sorted['freq_absolute'] - sorted['positive'].apply(lambda word: zipf_frequency(word, 'en')))
 
-print(sorted.tail(15))
 
+# Check if transition probability correlates with word frequency
+availability = sorted[['trans_prob', 'freq_absolute', 'freq_relative']]
+
+
+cond = availability['trans_prob'] > .12
+availability = availability[cond] # filter low prob transitions
+
+availability_check = availability.corr() # calculate the correlation
+# print(availability_check)
+
+# sns.set()
+# sns.relplot(x="freq_absolute", y="trans_prob", data=availability);
+# plt.show()
+
+# print(stats.pearsonr(availability['trans_prob'], availability['freq_absolute']))
+# print(stats.linregress(sorted['trans_prob'], sorted['freq_relative']))
 
 '''
 NOTES:
+
+
+    - NEXT TODO: Check if the absolute/relative frequency can predict the usage of
+        morphological antonyms
+
     - it is likely worth checking data for typos
         - eg ID1: uncreaitive, unresonable, unfoorgiving
         - maybe worth removing responses that weren't one word (eg not tolerant)
@@ -113,24 +138,11 @@ NOTES:
     - Is there a significant difference in word frequency of positive and negative adjectives?
         - What is the best way to quantify
 
+    - Very useful regression examples using sklearn
+        - https://nbviewer.jupyter.org/github/justmarkham/DAT4/blob/master/notebooks/08_linear_regression.ipynb
+
 
 ASK MH:
     - Do you know of good data for word frequencies?
         - https://www.wordandphrase.info/frequencyList.asp
 '''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# When I return, look at:
-# http://propor2016.di.fc.ul.pt/wp-content/uploads/2016/07/BrunaThalenbergPROPORSRW2016.pdf
